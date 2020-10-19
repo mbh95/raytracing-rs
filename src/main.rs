16c9ab7a112ab9a14cd026139583d517f68a4f62
mbh95 <matthewbh95@gmail.com>
2020-10-18 19:59:24 -0700

    Initial commit.
    Basic ppm image output from Ray Tracing in one weekend (section 2).

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::cmp::{max, min};
+
+
+fn unit_to_max(x: f64, max_value: u32) -> u32 {
+    let i = (x * max_value as f64) as i32;
+    min(max(i, 0) as u32, max_value)
+}
+
+fn main() {
+    let image_width = 256;
+    let image_height = 256;
+    println!("P3");
+    println!("{} {}", image_width, image_height);
+    println!("255");
+    for y in (0..image_height).rev() {
+        for x in 0..image_width {
+            let r = x as f64 / (image_width - 1) as f64;
+            let g = y as f64 / (image_height - 1) as f64;
+            let b = 0.25;
+
+            let ir = unit_to_max(r, 255);
+            let ig = unit_to_max(g, 255);
+            let ib = unit_to_max(b, 255);
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+}

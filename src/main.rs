af7a5dc07d18c0c75407625f79952dc78735ac42
mbh95 <matthewbh95@gmail.com>
2020-10-18 23:19:46 -0700

    Implement Vec3.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
-use std::cmp::{max, min};
+mod vec3;
 
+use crate::vec3::Vec3;
+use std::cmp::{max, min};
+use std::io::Write;
+
+type Color = Vec3;
 
 fn unit_to_max(x: f64, max_value: u32) -> u32 {
     let i = (x * max_value as f64) as i32;
     min(max(i, 0) as u32, max_value)
 }
 
-fn main() {
+fn write_color(o: &mut dyn Write, c: Color) -> Result<(), String> {
+    writeln!(
+        o,
+        "{} {} {}",
+        unit_to_max(c.x, 255),
+        unit_to_max(c.y, 255),
+        unit_to_max(c.z, 255)
+    )
+    .map_err(|e| e.to_string())
+}
+
+fn main() -> Result<(), String> {
     let image_width = 256;
     let image_height = 256;
     println!("P3");
@@ -15,18 +31,18 @@ fn main() {
     for y in (0..image_height).rev() {
         let remaining = y + 1;
         let percent_done = 100.0 * (image_height - remaining) as f64 / (image_height) as f64;
-        eprint!("\rScanlines remaining: {} ({:.1}%)", remaining, percent_done);
+        eprint!(
+            "\rScanlines remaining: {} ({:.1}%)",
+            remaining, percent_done
+        );
         for x in 0..image_width {
             let r = x as f64 / (image_width - 1) as f64;
             let g = y as f64 / (image_height - 1) as f64;
             let b = 0.25;
 
-            let ir = unit_to_max(r, 255);
-            let ig = unit_to_max(g, 255);
-            let ib = unit_to_max(b, 255);
-
-            println!("{} {} {}", ir, ig, ib);
+            write_color(&mut std::io::stdout(), Vec3::new(r, g, b))?;
         }
     }
     eprintln!("\rScanlines remaining: 0 ({:.1}%)\nDone!", 100.0);
+    Ok(())
 }
---
+++ src/vec3.rs
@@ -0,0 +1,204 @@
+use std::ops::{Add, Div, Mul, Neg, Sub};
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+pub struct Vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+#[allow(dead_code)]
+pub static ZERO: Vec3 = Vec3 {
+    x: 0.0,
+    y: 0.0,
+    z: 0.0,
+};
+
+#[allow(dead_code)]
+pub static X: Vec3 = Vec3 {
+    x: 1.0,
+    y: 0.0,
+    z: 0.0,
+};
+
+#[allow(dead_code)]
+pub static Y: Vec3 = Vec3 {
+    x: 0.0,
+    y: 1.0,
+    z: 0.0,
+};
+
+#[allow(dead_code)]
+pub static Z: Vec3 = Vec3 {
+    x: 0.0,
+    y: 0.0,
+    z: 1.0,
+};
+
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
+        Vec3 { x, y, z }
+    }
+
+    pub fn from_f64(e: f64) -> Vec3 {
+        Vec3::new(e, e, e)
+    }
+
+    pub fn len_sq(&self) -> f64 {
+        (self.x * self.x) + (self.y * self.y) + (self.z * self.z)
+    }
+
+    pub fn len(&self) -> f64 {
+        self.len_sq().sqrt()
+    }
+
+    pub fn norm(&self) -> Result<Vec3, String> {
+        let len_sq = self.len_sq();
+        if len_sq == 0.0 {
+            return Err("Tried to normalize a zero-length vector.".to_string());
+        } else if len_sq == 1.0 {
+            return Ok(*self);
+        }
+        Ok(*self / len_sq.sqrt())
+    }
+
+    pub fn dot(&self, rhs: &Vec3) -> f64 {
+        (self.x * rhs.x) + (self.y * rhs.y) + (self.z * rhs.z)
+    }
+
+    pub fn cross(&self, rhs: &Vec3) -> Vec3 {
+        let x = (self.y * rhs.z) - (self.z * rhs.y);
+        let y = (self.x * rhs.z) - (self.z * rhs.x);
+        let z = (self.x * rhs.y) - (self.y * rhs.x);
+        Vec3::new(x, y, z)
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Vec3 {
+        Vec3 {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f64) -> Vec3 {
+        Vec3 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        return rhs * self;
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Vec3 {
+        Vec3 {
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs,
+        }
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn len() {
+        let a = Vec3::new(1.0, 2.0, 3.0);
+        assert_eq!(a.len_sq(), 14.0);
+        assert_eq!(a.len(), 14.0f64.sqrt());
+    }
+
+    #[test]
+    fn norm() -> Result<(), String> {
+        let a = Vec3::new(1.0, 2.0, 3.0);
+        assert_eq!(a.norm()?.len_sq(), 1.0);
+        Ok(())
+    }
+
+    #[test]
+    fn neg() {
+        let a = Vec3::new(1.0, 2.0, 3.0);
+        assert_eq!(-a, Vec3::new(-1.0, -2.0, -3.0));
+    }
+
+    #[test]
+    fn mul() {
+        let v = Vec3::new(1.0, 2.0, 3.0);
+        let s = 2.0;
+        let r = Vec3::new(2.0, 4.0, 6.0);
+        assert_eq!(v * s, r);
+        assert_eq!(s * v, r);
+    }
+
+    #[test]
+    fn div() {
+        let a = Vec3::new(2.0, 4.0, 6.0);
+        assert_eq!(a / 2.0, Vec3::new(1.0, 2.0, 3.0));
+    }
+
+    #[test]
+    fn add() {
+        let a = Vec3::new(1.0, 2.0, 3.0);
+        let b = Vec3::new(10.0, 20.0, 30.0);
+        assert_eq!(a + b, Vec3::new(11.0, 22.0, 33.0));
+    }
+
+    #[test]
+    fn dot() {
+        let a = Vec3::new(1.0, 2.0, 3.0);
+        let b = Vec3::new(10.0, 20.0, 30.0);
+        assert_eq!(a.dot(&b), 140.0);
+        assert_eq!(a.dot(&ZERO), 0.0);
+    }
+
+    #[test]
+    fn cross() {
+        assert_eq!(X.cross(&Y), Z);
+        assert_eq!(ZERO.cross(&Y), ZERO);
+    }
+}
